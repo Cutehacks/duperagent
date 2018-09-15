24141e142bf56d8473a36d2ec5218d4647abe2d6
Rajesh Kumar Mallah <mallah.rajesh@gmail.com>
2018-09-15 23:49:32 +0530

    feature for handling gzip content encoding
    	gzip content encoding can greatly enhance the performance of
    	applications by reducing the data payload in the wire. This
    	commit utilizes zlib to decompress a compressed response of
    	gzip format.

--- response.cpp
+++ response.cpp
@@ -5,6 +5,8 @@
 #include <QtCore/QTextCodec>
 #include <QtNetwork/QNetworkReply>
 #include <QtQml/QQmlEngine>
+#include <QByteArray>
+#include <zlib.h>
 
 #include "response.h"
 #include "serialization.h"
@@ -16,6 +18,7 @@ ResponsePrototype::ResponsePrototype(QQmlEngine *engine, QNetworkReply *reply) :
     m_reply(reply)
 {
     QString type = m_reply->header(QNetworkRequest::ContentTypeHeader).toString();
+    QByteArray contentEncoding = m_reply->rawHeader(QByteArray ("Content-Encoding"));
 
     QRegExp charsetRegexp(".*charset=(.*)[\\s]*", Qt::CaseInsensitive, QRegExp::RegExp2);
     if (charsetRegexp.exactMatch(type)) {
@@ -23,7 +26,16 @@ ResponsePrototype::ResponsePrototype(QQmlEngine *engine, QNetworkReply *reply) :
     }
 
     if (m_reply->isReadable()) {
-        QByteArray data = m_reply->readAll();
+        QByteArray rawdata = m_reply->readAll();
+        QByteArray data;
+        if (contentEncoding  == QString("gzip")) {
+            if(! gzipDecompress(rawdata, data))
+            {
+            // TODO: add error handling
+            }
+        } else {
+            data  = rawdata;
+        }
 
         QTextCodec *text = QTextCodec::codecForName(m_charset.toLatin1());
         m_text = text ? text->makeDecoder()->toUnicode(data) : QString::fromUtf8(data);
@@ -124,5 +136,98 @@ QJSValue ResponsePrototype::header() const
 {
     return m_header;
 }
+
+bool ResponsePrototype::gzipDecompress( QByteArray input, QByteArray &output )
+{
+
+    // Prepare output
+    output.clear();
+
+    // Is there something to do?
+    if(input.length() > 0)
+    {
+        // Prepare inflater status
+        z_stream strm;
+        strm.zalloc = Z_NULL;
+        strm.zfree = Z_NULL;
+        strm.opaque = Z_NULL;
+        strm.avail_in = 0;
+        strm.next_in = Z_NULL;
+
+        // Initialize inflater
+        int ret = inflateInit2(&strm, GZIP_WINDOWS_BIT);
+
+        if (ret != Z_OK)
+            return(false);
+
+        // Extract pointer to input data
+        char *input_data = input.data();
+        int input_data_left = input.length();
+
+        // Decompress data until available
+        do {
+            // Determine current chunk size
+            int chunk_size = qMin(GZIP_CHUNK_SIZE, input_data_left);
+
+            // Check for termination
+            if(chunk_size <= 0)
+                break;
+
+            // Set inflater references
+            strm.next_in = (unsigned char*)input_data;
+            strm.avail_in = chunk_size;
+
+            // Update interval variables
+            input_data += chunk_size;
+            input_data_left -= chunk_size;
+
+            // Inflate chunk and cumulate output
+            do {
+
+                // Declare vars
+                char out[GZIP_CHUNK_SIZE];
+
+                // Set inflater references
+                strm.next_out = (unsigned char*)out;
+                strm.avail_out = GZIP_CHUNK_SIZE;
+
+                // Try to inflate chunk
+                ret = inflate(&strm, Z_NO_FLUSH);
+
+                switch (ret) {
+                case Z_NEED_DICT:
+                    ret = Z_DATA_ERROR;
+                case Z_DATA_ERROR:
+                case Z_MEM_ERROR:
+                case Z_STREAM_ERROR:
+                    // Clean-up
+                    inflateEnd(&strm);
+
+                    // Return
+                    return(false);
+                }
+
+                // Determine decompressed size
+                int have = (GZIP_CHUNK_SIZE - strm.avail_out);
+
+                // Cumulate result
+                if(have > 0)
+                    output.append((char*)out, have);
+
+            } while (strm.avail_out == 0);
+
+        } while (ret != Z_STREAM_END);
+
+        // Clean-up
+        inflateEnd(&strm);
+
+        // Return
+        return (ret == Z_STREAM_END);
+    }
+    else
+        return(true);
+}
+
+
 
 } } }
